500acd53f77986a3ba70066dbf7c6c41a9be5987
Lakota Camp <lakotacamp3@gmail.com>
2024-05-23 13:01:17 -0700

    added stringUtils.js - Currently on React Router section

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,15 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
-
-// Pet component
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import SearchParams from "./SearchParams";
 
 // App component
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      animal: "Dog",
-      name: "Petey",
-      breed: "Boston Terrier & Pug",
-    }),
-
-    React.createElement(Pet, {
-      animal: "Cat",
-      name: "Phil",
-      breed: "Mixed",
-    }),
-    React.createElement(Pet, {
-      animal: "Dog",
-      name: "Booker",
-      breed: "Australian Shepherd",
-    }),
-    React.createElement(Pet, {
-      animal: "Cat",
-      name: "Charlie",
-      breed: "Mixed",
-    }),
-    React.createElement(Pet, {
-      animal: "Bird",
-      name: "Coconut",
-      breed: "Parakeet",
-    }),
-    React.createElement(Pet, {
-      animal: "Bird",
-      name: "Pineapple",
-      breed: "Parakeet",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
---
+++ src/Pet.jsx
@@ -0,0 +1,26 @@
+import { capitalizeFirstLetter } from "../utils/stringUtils";
+
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = "http://pets-images.dev-apis.com/pets/non.jpeg";
+  if (images.length) {
+    hero = images[0];
+  }
+
+  // Pet component
+
+  return (
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>
+          {capitalizeFirstLetter(animal)} - {breed} - {location}
+        </h2>
+      </div>
+    </a>
+  );
+};
+
+export default Pet;
