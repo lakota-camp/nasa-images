8ded92556dfe1cd8703253466e3803fb01939cd0
Lakota Camp <lakotacamp3@gmail.com>
2024-08-02 09:21:44 -0700

    added build process

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: "src", // sets path of root html
+  build: {
+    outDir: "../dist", // Output directory for the build
+    emptyOutDir: true, // Clear the output directory before building
+  },
 });
